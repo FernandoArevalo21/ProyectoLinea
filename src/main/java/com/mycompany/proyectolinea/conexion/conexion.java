68c725be18c53cbfe056b66b1992e0f187eb7e81
FernandoArevalo21 <fernandoarevalo2000@hotmail.com>
2020-11-15 23:49:33 -0500

    Se crea la conexión a la bd

---
+++ src/main/java/com/mycompany/proyectolinea/conexion/conexion.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyectolinea.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author usuario
+ */
+public class conexion {
+     Connection conect = null;
+    public Connection conexion() throws SQLException  {
+        System.out.println("conexión"+conect);
+         try {
+                //Cargamos el Driver MySQL
+                Class.forName("com.mysql.jdbc.Driver");
+                //Se hace la conexión con el localhost y con la base de datos creada que en éste caso se llamí agata y "root" que por lo general es el usuario de ingreso
+                conect = DriverManager.getConnection("jdbc:mysql://localhost/proyectolinea","root","");
+              System.out.println("conexion establecida");
+            } catch (ClassNotFoundException | SQLException e) {
+                System.out.println("error de conexion");
+                //JOptionPane.showMessageDialog(null,"Error de conexion"+e);
+            }
+            return conect;
+            
+    }
+}
